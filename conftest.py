ef8c63a874e361092cecb36de9242d82cf86c93f
JelenaG <daush.da@gmail.com>
2021-05-24 12:38:21 +0300

    initial commit for test framework

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or safari")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: en or ru")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "safari":
+        print("\nstart safari browser for test..")
+        browser = webdriver.Safari()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or safari")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
