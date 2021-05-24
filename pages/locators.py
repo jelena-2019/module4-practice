72a6a35d182b1004a7dc96a89b007a76108ac48a
JelenaG <daush.da@gmail.com>
2021-05-24 13:52:18 +0300

    locators page added

---
+++ pages/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK_promo = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
